0660cca32ff8104f1a819aed6849cdf520c4bb9d
MrOsteoporosis <sanderverheijen@outlook.com>
2021-01-12 15:01:31 +0100

    ex01 removed excess includes

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,13 +6,11 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/12 11:33:32 by averheij      #+#    #+#                 */
-/*   Updated: 2021/01/12 14:04:07 by averheij      ########   odam.nl         */
+/*   Updated: 2021/01/12 15:01:08 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
-#include "phonebook.class.hpp"
-#include "contact.class.hpp"
 
 void	testContact(void) {
 	Contact contact;
